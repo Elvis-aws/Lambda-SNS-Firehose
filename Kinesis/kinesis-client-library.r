54de0bfb2e24fed9e56e44c4993d85ee9228d4f1
Elvis Ngwesse <elvisngwesseaws@gmail.com>
2022-12-28 14:14:56 +0000

    Updates

---
+++ Kinesis/kinesis-client-library.r
@@ -0,0 +1,8 @@
+
+
+***
+KCL
+***
+    - A Java library that helps read record from Kinesis Data Stream 
+    - Each shard is to be read by 1 KCL instance
+    - They read and store data into DynamoDB
---
+++ Kinesis/shard-splitting.r
@@ -0,0 +1,9 @@
+
+****************
+Shards Splitting
+****************
+    - Used to increase the Stream capacity
+    - Manually increase or decrease capacity
+    - No more than 2 shard splitting
+    - We can merge shards into one to decrease cost
+    - Old shard will be deleted
